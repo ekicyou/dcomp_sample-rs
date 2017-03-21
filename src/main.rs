f331828d7c2c75904c82b4c33f2c2d3ab4df4985
ekicyou <dot.station@gmail.com>
2017-03-21 17:46:28 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+#![cfg(windows)]
+
 extern crate winapi;
+extern crate winit;
 
 fn main() {
-    println!("はろーわーるど");
+    let window = winit::Window::new().unwrap();
+
+    for event in window.wait_events() {
+        match event {
+            winit::Event::Closed => break,
+            _ => (),
+        }
+    }
 }
 
 #[cfg(test)]
