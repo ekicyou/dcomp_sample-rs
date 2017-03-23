055dcacf58a7e2a5e96528eacea227553516fa03
ekicyou <dot.station@gmail.com>
2017-03-23 18:01:49 +0900

    com smart ptr

--- src/comptr.rs
+++ src/comptr.rs
@@ -1,17 +1,26 @@
 use winapi::_core::ops::{Deref, DerefMut};
+use winapi::_core as core;
+use winapi::Interface;
 use winapi::shared::wtypesbase::ULONG;
-use winapi::shared::winerror::{HRESULT, E_FAIL};
+use winapi::shared::winerror::{HRESULT, S_OK, E_FAIL};
 use winapi::um::unknwnbase::IUnknown;
 
 pub trait IUnknownInterface {
-    unsafe fn query_interface<T: IUnknownInterface>(&self) -> Result<T, HRESULT>;
+    unsafe fn query_interface<T: IUnknownInterface + Interface>(&self) -> Result<*mut T, HRESULT>;
     unsafe fn add_ref(&mut self) -> ULONG;
     unsafe fn release(&mut self) -> ULONG;
 }
 
 impl IUnknownInterface for IUnknown {
-    unsafe fn query_interface<T: IUnknownInterface>(&self) -> Result<T, HRESULT> {
-        Err(E_FAIL)
+    unsafe fn query_interface<T: IUnknownInterface + Interface>(&self) -> Result<*mut T, HRESULT> {
+        let guid = T::uuidof();
+        let mut ptr = core::ptr::null_mut();
+        unsafe {
+            match self.QueryInterface(&guid, &mut ptr) {
+                S_OK => Ok(ptr as *mut T),
+                hr => Err(hr),
+            }
+        }
     }
     unsafe fn add_ref(&mut self) -> ULONG {
         let count = self.AddRef();
@@ -23,32 +32,31 @@ impl IUnknownInterface for IUnknown {
     }
 }
 
-pub struct ComPtr<T: IUnknownInterface> {
+pub struct ComPtr<T: IUnknownInterface + Interface> {
     raw: T,
 }
 
-impl<T: IUnknownInterface> ComPtr<T> {
+impl<T: IUnknownInterface + Interface> ComPtr<T> {
     fn new(mut com: T) -> ComPtr<T> {
         unsafe { com.add_ref() };
         ComPtr { raw: com }
     }
 }
 
-impl<T: IUnknownInterface> Drop for ComPtr<T> {
+impl<T: IUnknownInterface + Interface> Drop for ComPtr<T> {
     fn drop(&mut self) {
         unsafe { self.raw.release() };
     }
 }
 
-impl<T: IUnknownInterface> Deref for ComPtr<T> {
+impl<T: IUnknownInterface + Interface> Deref for ComPtr<T> {
     type Target = T;
     fn deref(&self) -> &T {
-        let com = &self.raw;
-        com
+        &self.raw
     }
 }
 
-impl<T: IUnknownInterface> DerefMut for ComPtr<T> {
+impl<T: IUnknownInterface + Interface> DerefMut for ComPtr<T> {
     fn deref_mut(&mut self) -> &mut T {
         &mut self.raw
     }
