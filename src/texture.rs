697508b655a2f948105005ca9569d804ea6dc958
ekicyou <dot.station@gmail.com>
2017-06-16 15:34:50 +0900

    リファクタリング

--- src/texture.rs
+++ src/texture.rs
@@ -9,20 +9,20 @@ pub fn generate_texture_data() -> Vec<u8> {
     let cell_height = (TEXTURE_WIDTH >> 3) as usize; // The height of a cell in the checkerboard texture.
     let texture_size = row_pitch * TEXTURE_HEIGHT as usize;
 
-    let colors = [
-        XMFLOAT4::new(1_f32, 0_f32, 0_f32, 1_f32), // Red
-        XMFLOAT4::new(0_f32, 1_f32, 0_f32, 1_f32), // Green
-        XMFLOAT4::new(0_f32, 0_f32, 1_f32, 1_f32), // Blue
-        XMFLOAT4::new(0_f32, 0_f32, 0_f32, 1_f32), // Black
-        XMFLOAT4::new(1_f32, 1_f32, 1_f32, 1_f32), // White
-        XMFLOAT4::new(1_f32, 1_f32, 0_f32, 1_f32), // Yellow
-        XMFLOAT4::new(0_f32, 1_f32, 1_f32, 1_f32), // Cyan
-        XMFLOAT4::new(1_f32, 0_f32, 1_f32, 1_f32)  // Purple
-    ];
 
     let mut buf: Vec<u8> = Vec::new();
     buf.resize(texture_size, 0_u8);
     {
+        let colors = [
+            XMFLOAT4::new(1_f32, 0_f32, 0_f32, 1_f32), // Red
+            XMFLOAT4::new(0_f32, 1_f32, 0_f32, 1_f32), // Green
+            XMFLOAT4::new(0_f32, 0_f32, 1_f32, 1_f32), // Blue
+            XMFLOAT4::new(0_f32, 0_f32, 0_f32, 1_f32), // Black
+            XMFLOAT4::new(1_f32, 1_f32, 1_f32, 1_f32), // White
+            XMFLOAT4::new(1_f32, 1_f32, 0_f32, 1_f32), // Yellow
+            XMFLOAT4::new(0_f32, 1_f32, 1_f32, 1_f32), // Cyan
+            XMFLOAT4::new(1_f32, 0_f32, 1_f32, 1_f32)  // Purple
+        ];
         let mut data = buf.as_mut_slice();
         for a in 0_usize..NUM_ALPHA_SHADES as _ {
             let alpha = (a as f32) / ((NUM_ALPHA_SHADES - 1) as f32);
