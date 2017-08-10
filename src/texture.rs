9854af388e19ab1d6127dceedcf367efc75f462a
ekicyou <dot.station@gmail.com>
2017-08-10 14:59:24 +0900

    リファクタリング

--- src/texture.rs
+++ src/texture.rs
@@ -5,23 +5,27 @@ use winapi::_core::mem;
 // Generate a simple black and white checkerboard texture.
 pub fn generate_texture_data() -> Vec<u8> {
     let row_pitch = mem::size_of::<u32>() * TEXTURE_WIDTH as usize;
-    let cell_pitch = (row_pitch >> 3) as usize; // The width of a cell in the checkboard texture.
-    let cell_height = (TEXTURE_WIDTH >> 3) as usize; // The height of a cell in the checkerboard texture.
+    //let cell_pitch = (row_pitch >> 3) as usize; // The width of a cell in the checkboard texture.
+    //let cell_height = (TEXTURE_WIDTH >> 3) as usize; // The height of a cell in the checkerboard texture.
     let texture_size = row_pitch * TEXTURE_HEIGHT as usize;
     let mut buf: Vec<u8> = Vec::new();
     buf.resize(texture_size, 0_u8);
     {
         let mut data = buf.as_mut_slice();
-        let colors = [
-            XMFLOAT4::new(1_f32, 0_f32, 0_f32, 1_f32), // Red
-            XMFLOAT4::new(0_f32, 1_f32, 0_f32, 1_f32), // Green
-            XMFLOAT4::new(0_f32, 0_f32, 1_f32, 1_f32), // Blue
-            XMFLOAT4::new(0_f32, 0_f32, 0_f32, 1_f32), // Black
-            XMFLOAT4::new(1_f32, 1_f32, 1_f32, 1_f32), // White
-            XMFLOAT4::new(1_f32, 1_f32, 0_f32, 1_f32), // Yellow
-            XMFLOAT4::new(0_f32, 1_f32, 1_f32, 1_f32), // Cyan
-            XMFLOAT4::new(1_f32, 0_f32, 1_f32, 1_f32)  // Purple
-        ];
+        let colors = {
+            let m = 1_f32;
+            let i = 0_f32;
+            [
+                XMFLOAT4::new(m, i, i, m), // Red
+                XMFLOAT4::new(i, m, i, m), // Green
+                XMFLOAT4::new(i, i, m, m), // Blue
+                XMFLOAT4::new(i, i, i, m), // Black
+                XMFLOAT4::new(m, m, m, m), // White
+                XMFLOAT4::new(m, m, i, m), // Yellow
+                XMFLOAT4::new(i, m, m, m), // Cyan
+                XMFLOAT4::new(m, i, m, m), // Purple
+            ]
+        };
         for a in 0_usize..NUM_ALPHA_SHADES as _ {
             let alpha = (a as f32) / ((NUM_ALPHA_SHADES - 1) as f32);
             let start_x = a * TEXTURE_PIXEL_SIZE_X as usize;
