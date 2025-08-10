aa12755e107f84359ca959a9b5ad7970e5bdf6e5
ekicyou <dot.station@gmail.com>
2025-08-10 20:37:07 +0900

    ウィンドウメッセージハンドラのトレイトを追加し、メッセージ処理の実装を整備

---
+++ src/win_message_handler.rs
@@ -0,0 +1,57 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+use windows::Win32::Foundation::*;
+use windows::Win32::UI::WindowsAndMessaging::*;
+
+pub trait WindowMessageHandler {
+    #[inline(always)]
+    fn message_handler(
+        &mut self,
+        hwnd: HWND,
+        message: u32,
+        wparam: WPARAM,
+        lparam: LPARAM,
+    ) -> LRESULT {
+        match message {
+            WM_LBUTTONUP => self.WM_LBUTTONUP(wparam, lparam),
+            WM_PAINT => self.WM_PAINT(wparam, lparam),
+            WM_DPICHANGED => self.WM_DPICHANGED(wparam, lparam),
+            WM_CREATE => self.WM_CREATE(wparam, lparam),
+            WM_WINDOWPOSCHANGING => self.WM_WINDOWPOSCHANGING(wparam, lparam),
+            WM_DESTROY => self.WM_DESTROY(wparam, lparam),
+            _ => return unsafe { DefWindowProcA(hwnd, message, wparam, lparam) },
+        }
+    }
+
+    #[inline]
+    fn WM_LBUTTONUP(&mut self, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+        LRESULT(0)
+    }
+
+    #[inline]
+    fn WM_PAINT(&mut self, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+        LRESULT(0)
+    }
+
+    #[inline]
+    fn WM_DPICHANGED(&mut self, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+        LRESULT(0)
+    }
+
+    #[inline]
+    fn WM_CREATE(&mut self, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+        LRESULT(0)
+    }
+
+    #[inline]
+    fn WM_WINDOWPOSCHANGING(&mut self, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+        LRESULT(0)
+    }
+
+    #[inline]
+    fn WM_DESTROY(&mut self, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+        LRESULT(0)
+    }
+}
