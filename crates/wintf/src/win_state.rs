8bccf3ea7288a50a3fbb2cb7fc6693c32cc6224c
岡本 <okamoto_masakazu-tch@global.kawasaki.com>
2025-09-11 17:33:48 +0900

    Dpi構造体とDpiValue構造体を追加し、DPIの設定と変換メソッドを実装

--- crates/wintf/src/win_state.rs
+++ crates/wintf/src/win_state.rs
@@ -3,6 +3,48 @@
 use ambassador::*;
 use windows::Win32::Foundation::*;
 
+#[derive(Debug, Default, Clone, Copy)]
+pub struct DpiValue(f32);
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct Dpi {
+    pub x: DpiValue,
+    pub y: DpiValue,
+}
+
+impl Dpi {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self {
+            x: DpiValue(x),
+            y: DpiValue(y),
+        }
+    }
+
+    pub fn set(&mut self, dpi: (f32, f32)) {
+        self.x = DpiValue(dpi.0);
+        self.y = DpiValue(dpi.1);
+    }
+
+    pub fn set_dpi_from_message(&mut self, wparam: WPARAM, _lparam: LPARAM) {
+        let dpi = (wparam.0 as u16 as f32, (wparam.0 >> 16) as f32);
+        self.set(dpi);
+    }
+}
+
+impl DpiValue {
+    pub fn get(&self) -> f32 {
+        self.0
+    }
+
+    pub fn physical_to_logical(&self, pixel: f32) -> f32 {
+        pixel * 96.0 / self.get()
+    }
+
+    pub fn logical_to_physical(&self, pixel: f32) -> f32 {
+        pixel * self.get() / 96.0
+    }
+}
+
 #[delegatable_trait]
 pub trait WinState {
     fn hwnd(&self) -> HWND;
