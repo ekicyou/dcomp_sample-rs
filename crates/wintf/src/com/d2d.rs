a905fe41fbcf51577ab4cdb9128e3ff67e0155b3
ekicyou <dot.station@gmail.com>
2025-09-29 19:25:45 +0900

    DrawGdiMetafile構造体を追加し、関連する描画コマンドを整理。描画コマンドの列挙型を拡張し、描画関連の構造体を整理。

--- crates/wintf/src/com/d2d.rs
+++ crates/wintf/src/com/d2d.rs
@@ -2,6 +2,7 @@ use std::mem::*;
 use windows::core::*;
 use windows::Win32::Graphics::Direct2D::Common::*;
 use windows::Win32::Graphics::Direct2D::*;
+use windows::Win32::Graphics::DirectWrite::*;
 use windows::Win32::Graphics::Dxgi::*;
 use windows_numerics::*;
 
@@ -42,239 +43,194 @@ pub struct DrawInk {
 #[derive(Clone, Debug)]
 pub struct DrawGradientMesh(pub ManuallyDrop<ID2D1GradientMesh>);
 
-
-
+#[derive(Clone, Debug)]
+pub struct DrawGdiMetafile {
+    pub gdimetafile: ManuallyDrop<ID2D1GdiMetafile>,
+    pub destinationrectangle: Option<D2D_RECT_F>,
+    pub sourcerectangle: Option<D2D_RECT_F>,
+}
 
 #[derive(Clone, Debug)]
-pub unsafe fn DrawGdiMetafile<P0>(
-    &self,
-    gdimetafile: P0,
-    destinationrectangle: Option<*const D2D_RECT_F>,
-    sourcerectangle: Option<*const D2D_RECT_F>,
-) -> Result<()>
-where
-    P0: Param<ID2D1GdiMetafile>,
-Methods from Deref<Target = ID2D1CommandSink1>
-
-
+pub struct SetPrimitiveBlend1(pub D2D1_PRIMITIVE_BLEND);
 
 #[derive(Clone, Debug)]
-pub unsafe fn SetPrimitiveBlend1(
-    &self,
-    primitiveblend: D2D1_PRIMITIVE_BLEND,
-) -> Result<()>
-Methods from Deref<Target = ID2D1CommandSink>
-
+pub struct SetAntialiasMode(pub D2D1_ANTIALIAS_MODE);
 
 #[derive(Clone, Debug)]
-pub unsafe fn BeginDraw(&self) -> Result<()>
-
-
-#[derive(Clone, Debug)]
-pub unsafe fn EndDraw(&self) -> Result<()>
-
+pub struct SetTags {
+    pub tag1: u64,
+    pub tag2: u64,
+}
 
 #[derive(Clone, Debug)]
-pub unsafe fn SetAntialiasMode(
-    &self,
-    antialiasmode: D2D1_ANTIALIAS_MODE,
-) -> Result<()>
-
+pub struct SetTextAntialiasMode(pub D2D1_TEXT_ANTIALIAS_MODE);
 
 #[derive(Clone, Debug)]
-pub unsafe fn SetTags(&self, tag1: u64, tag2: u64) -> Result<()>
-
-
-#[derive(Clone, Debug)]
-pub unsafe fn SetTextAntialiasMode(
-    &self,
-    textantialiasmode: D2D1_TEXT_ANTIALIAS_MODE,
-) -> Result<()>
-
+pub struct SetTextRenderingParams {
+    pub textrenderingparams: ManuallyDrop<IDWriteRenderingParams>,
+}
 
 #[derive(Clone, Debug)]
-pub unsafe fn SetTextRenderingParams<P0>(
-    &self,
-    textrenderingparams: P0,
-) -> Result<()>
-where
-    P0: Param<IDWriteRenderingParams>,
-
+pub struct SetTransform(pub Matrix3x2);
 
 #[derive(Clone, Debug)]
-    pub unsafe fn SetTransform(&self, transform: *const Matrix3x2) -> Result<()>
-
-
-#[derive(Clone, Debug)]
-    pub unsafe fn SetPrimitiveBlend(
-    &self,
-    primitiveblend: D2D1_PRIMITIVE_BLEND,
-) -> Result<()>
-
+pub struct SetPrimitiveBlend(pub D2D1_PRIMITIVE_BLEND);
 
 #[derive(Clone, Debug)]
-pub unsafe fn SetUnitMode(&self, unitmode: D2D1_UNIT_MODE) -> Result<()>
-
-
-#[derive(Clone, Debug)]
-pub unsafe fn Clear(&self, color: Option<*const D2D1_COLOR_F>) -> Result<()>
-
+pub struct SetUnitMode(pub D2D1_UNIT_MODE);
 
 #[derive(Clone, Debug)]
-pub unsafe fn DrawGlyphRun<P3>(
-    &self,
-    baselineorigin: D2D_POINT_2F,
-    glyphrun: *const DWRITE_GLYPH_RUN,
-    glyphrundescription: Option<*const DWRITE_GLYPH_RUN_DESCRIPTION>,
-    foregroundbrush: P3,
-    measuringmode: DWRITE_MEASURING_MODE,
-) -> Result<()>
-where
-    P3: Param<ID2D1Brush>,
-
+pub struct Clear(pub Option<D2D1_COLOR_F>);
 
 #[derive(Clone, Debug)]
-    pub unsafe fn DrawLine<P2, P4>(
-    &self,
-    point0: D2D_POINT_2F,
-    point1: D2D_POINT_2F,
-    brush: P2,
-    strokewidth: f32,
-    strokestyle: P4,
-) -> Result<()>
-where
-    P2: Param<ID2D1Brush>,
-    P4: Param<ID2D1StrokeStyle>,
-
-
-#[derive(Clone, Debug)]
-    pub unsafe fn DrawGeometry<P0, P1, P3>(
-    &self,
-    geometry: P0,
-    brush: P1,
-    strokewidth: f32,
-    strokestyle: P3,
-) -> Result<()>
-where
-    P0: Param<ID2D1Geometry>,
-    P1: Param<ID2D1Brush>,
-    P3: Param<ID2D1StrokeStyle>,
-
+pub struct DrawGlyphRun {
+    pub baselineorigin: Vector2,
+    // --- DWRITE_GLYPH_RUN START ---
+    pub font_face: ManuallyDrop<IDWriteFontFace>,
+    pub font_em_size: f32,
+    pub glyph_indices: Vec<u16>,
+    pub glyph_advances: Vec<f32>,
+    pub glyph_offsets: Vec<DWRITE_GLYPH_OFFSET>,
+    pub is_sideways: bool,
+    pub bidi_level: u32,
+    // --- DWRITE_GLYPH_RUN END ---
+    pub glyphrundescription: Option<DWRITE_GLYPH_RUN_DESCRIPTION>,
+    pub foregroundbrush: ManuallyDrop<ID2D1Brush>,
+    pub measuringmode: DWRITE_MEASURING_MODE,
+}
 
 #[derive(Clone, Debug)]
-    pub unsafe fn DrawRectangle<P1, P3>(
-    &self,
-    rect: *const D2D_RECT_F,
-    brush: P1,
-    strokewidth: f32,
-    strokestyle: P3,
-) -> Result<()>
-where
-    P1: Param<ID2D1Brush>,
-    P3: Param<ID2D1StrokeStyle>,
-
+pub struct DrawLine {
+    pub point0: Vector2,
+    pub point1: Vector2,
+    pub brush: ManuallyDrop<ID2D1Brush>,
+    pub strokewidth: f32,
+    pub strokestyle: ManuallyDrop<ID2D1StrokeStyle>,
+}
 
 #[derive(Clone, Debug)]
-    pub unsafe fn DrawBitmap<P0>(
-    &self,
-    bitmap: P0,
-    destinationrectangle: Option<*const D2D_RECT_F>,
-    opacity: f32,
-    interpolationmode: D2D1_INTERPOLATION_MODE,
-    sourcerectangle: Option<*const D2D_RECT_F>,
-    perspectivetransform: Option<*const D2D_MATRIX_4X4_F>,
-) -> Result<()>
-where
-    P0: Param<ID2D1Bitmap>,
-
+pub struct DrawGeometry {
+    pub geometry: ManuallyDrop<ID2D1Geometry>,
+    pub brush: ManuallyDrop<ID2D1Brush>,
+    pub strokewidth: f32,
+    pub strokestyle: ManuallyDrop<ID2D1StrokeStyle>,
+}
 
 #[derive(Clone, Debug)]
-    pub unsafe fn DrawImage<P0>(
-    &self,
-    image: P0,
-    targetoffset: Option<*const D2D_POINT_2F>,
-    imagerectangle: Option<*const D2D_RECT_F>,
-    interpolationmode: D2D1_INTERPOLATION_MODE,
-    compositemode: D2D1_COMPOSITE_MODE,
-) -> Result<()>
-where
-    P0: Param<ID2D1Image>,
-
+pub struct DrawRectangle {
+    pub rect: D2D_RECT_F,
+    pub brush: ManuallyDrop<ID2D1Brush>,
+    pub strokewidth: f32,
+    pub strokestyle: ManuallyDrop<ID2D1StrokeStyle>,
+}
 
 #[derive(Clone, Debug)]
-    pub unsafe fn DrawGdiMetafile<P0>(
-    &self,
-    gdimetafile: P0,
-    targetoffset: Option<*const D2D_POINT_2F>,
-) -> Result<()>
-where
-    P0: Param<ID2D1GdiMetafile>,
-
-
-#[derive(Clone, Debug)]
-    pub unsafe fn FillMesh<P0, P1>(&self, mesh: P0, brush: P1) -> Result<()>
-where
-    P0: Param<ID2D1Mesh>,
-    P1: Param<ID2D1Brush>,
-
+pub struct DrawBitmap {
+    pub bitmap: ManuallyDrop<ID2D1Bitmap>,
+    pub destinationrectangle: Option<D2D_RECT_F>,
+    pub opacity: f32,
+    pub interpolationmode: D2D1_INTERPOLATION_MODE,
+    pub sourcerectangle: Option<D2D_RECT_F>,
+    pub perspectivetransform: Option<Matrix4x4>,
+}
 
 #[derive(Clone, Debug)]
-    pub unsafe fn FillOpacityMask<P0, P1>(
-    &self,
-    opacitymask: P0,
-    brush: P1,
-    destinationrectangle: Option<*const D2D_RECT_F>,
-    sourcerectangle: Option<*const D2D_RECT_F>,
-) -> Result<()>
-where
-    P0: Param<ID2D1Bitmap>,
-    P1: Param<ID2D1Brush>,
-
+pub struct DrawImage {
+    pub image: ManuallyDrop<ID2D1Image>,
+    pub targetoffset: Option<Vector2>,
+    pub imagerectangle: Option<D2D_RECT_F>,
+    pub interpolationmode: D2D1_INTERPOLATION_MODE,
+    pub compositemode: D2D1_COMPOSITE_MODE,
+}
 
 #[derive(Clone, Debug)]
-    pub unsafe fn FillGeometry<P0, P1, P2>(
-    &self,
-    geometry: P0,
-    brush: P1,
-    opacitybrush: P2,
-) -> Result<()>
-where
-    P0: Param<ID2D1Geometry>,
-    P1: Param<ID2D1Brush>,
-    P2: Param<ID2D1Brush>,
-
+pub struct DrawGdiMetafile2 {
+    pub gdimetafile: ManuallyDrop<ID2D1GdiMetafile>,
+    pub targetoffset: Option<Vector2>,
+}
 
 #[derive(Clone, Debug)]
-    pub unsafe fn FillRectangle<P1>(
-    &self,
-    rect: *const D2D_RECT_F,
-    brush: P1,
-) -> Result<()>
-where
-    P1: Param<ID2D1Brush>,
-
+pub struct FillMesh {
+    pub mesh: ManuallyDrop<ID2D1Mesh>,
+    pub brush: ManuallyDrop<ID2D1Brush>,
+}
 
 #[derive(Clone, Debug)]
-    pub unsafe fn PushAxisAlignedClip(
-    &self,
-    cliprect: *const D2D_RECT_F,
-    antialiasmode: D2D1_ANTIALIAS_MODE,
-) -> Result<()>
-
-
-#[derive(Clone, Debug)]
-pub unsafe fn PushLayer<P1>(
-    &self,
-    layerparameters1: *const D2D1_LAYER_PARAMETERS1,
-    layer: P1,
-) -> Result<()>
-where
-    P1: Param<ID2D1Layer>,
-
+pub struct FillOpacityMask {
+    pub opacitymask: ManuallyDrop<ID2D1Bitmap>,
+    pub brush: ManuallyDrop<ID2D1Brush>,
+    pub destinationrectangle: Option<D2D_RECT_F>,
+    pub sourcerectangle: Option<D2D_RECT_F>,
+}
 
 #[derive(Clone, Debug)]
-    pub unsafe fn PopAxisAlignedClip(&self) -> Result<()>
-
+pub struct FillGeometry {
+    pub geometry: ManuallyDrop<ID2D1Geometry>,
+    pub brush: ManuallyDrop<ID2D1Brush>,
+    pub opacitybrush: ManuallyDrop<ID2D1Brush>,
+}
 
 #[derive(Clone, Debug)]
-    pub unsafe fn PopLayer(&self) -> Result<()>
+pub struct FillRectangle {
+    pub rect: D2D_RECT_F,
+    pub brush: ManuallyDrop<ID2D1Brush>,
+}
+
+#[derive(Clone, Debug)]
+pub struct PushAxisAlignedClip {
+    pub cliprect: D2D_RECT_F,
+    pub antialiasmode: D2D1_ANTIALIAS_MODE,
+}
+
+#[derive(Clone, Debug)]
+pub struct PushLayer {
+    pub layerparameters1: D2D1_LAYER_PARAMETERS1,
+    pub layer: ManuallyDrop<ID2D1Layer>,
+}
+
+#[derive(Clone, Debug)]
+pub enum DrawCommand {
+    // 描画コマンド（状態を変更しない）
+    BlendImage(BlendImage),
+    DrawSpriteBatch(DrawSpriteBatch),
+    DrawInk(DrawInk),
+    DrawGradientMesh(DrawGradientMesh),
+    DrawGdiMetafile(DrawGdiMetafile),
+    DrawGlyphRun(DrawGlyphRun),
+    DrawLine(DrawLine),
+    DrawGeometry(DrawGeometry),
+    DrawRectangle(DrawRectangle),
+    DrawBitmap(DrawBitmap),
+    DrawImage(DrawImage),
+    DrawGdiMetafile2(DrawGdiMetafile2),
+    FillMesh(FillMesh),
+    FillOpacityMask(FillOpacityMask),
+    FillGeometry(FillGeometry),
+    FillRectangle(FillRectangle),
+    Clear(Clear),
+
+    // 描画開始/終了
+    BeginDraw,
+    EndDraw,
+
+    // 設定コマンド（状態を変更する）
+    SetPrimitiveBlend2(SetPrimitiveBlend2),
+    SetPrimitiveBlend1(SetPrimitiveBlend1),
+    SetPrimitiveBlend(SetPrimitiveBlend),
+
+    SetAntialiasMode(SetAntialiasMode),
+
+    SetTags(SetTags),
+
+    SetTextAntialiasMode(SetTextAntialiasMode),
+
+    SetTextRenderingParams(SetTextRenderingParams),
+    SetTransform(SetTransform),
+    SetUnitMode(SetUnitMode),
+
+    PushAxisAlignedClip(PushAxisAlignedClip),
+    PopAxisAlignedClip,
+
+    PushLayer(PushLayer),
+    PopLayer,
+}
