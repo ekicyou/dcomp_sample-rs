da68783c6ebfc4ac4f7ffe6472e9cc48a42457cb
岡本 <okamoto_masakazu-tch@global.kawasaki.com>
2025-09-30 14:43:43 +0900

    feat(dcomp): DCompositionCreateDevice3のドキュメントコメントを追加

--- crates/wintf/src/com/dcomp.rs
+++ crates/wintf/src/com/dcomp.rs
@@ -5,6 +5,7 @@ use windows::Win32::Graphics::Dxgi::Common::*;
 use windows::Win32::System::Com::*;
 use windows_numerics::*;
 
+/// DCompositionCreateDevice3
 pub fn dcomp_create_desktop_device<P0>(renderingdevice: P0) -> Result<IDCompositionDesktopDevice>
 where
     P0: Param<IUnknown>,
