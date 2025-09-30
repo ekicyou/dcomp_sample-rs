b07a92e5903b638d3500e6bc433ec8a7e6a770ba
岡本 <okamoto_masakazu-tch@global.kawasaki.com>
2025-09-30 15:50:10 +0900

    refactor(d2d): D2D1DeviceExtトレイトの名前を修正

--- crates/wintf/src/com/d2d/mod.rs
+++ crates/wintf/src/com/d2d/mod.rs
@@ -11,14 +11,14 @@ pub fn d2d_create_device(dxgi: &IDXGIDevice4) -> Result<ID2D1Device> {
     unsafe { D2D1CreateDevice(dxgi, None) }
 }
 
-pub trait ID2D1DeviceExt {
+pub trait D2D1DeviceExt {
     fn create_device_context(
         &self,
         options: D2D1_DEVICE_CONTEXT_OPTIONS,
     ) -> Result<ID2D1DeviceContext>;
 }
 
-impl ID2D1DeviceExt for ID2D1Device {
+impl D2D1DeviceExt for ID2D1Device {
     #[inline(always)]
     fn create_device_context(
         &self,
