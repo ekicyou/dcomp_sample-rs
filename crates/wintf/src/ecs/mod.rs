b3cdd68508cdd7fc3a4c64d7c6960f2a2451c5a5
ekicyou <dot.station@gmail.com>
2025-11-11 19:16:04 +0900

    feat: ウィンドウプロシージャを新規作成し、EcsWorldとの連携を実装

--- crates/wintf/src/ecs/mod.rs
+++ crates/wintf/src/ecs/mod.rs
@@ -2,11 +2,13 @@ mod app;
 mod graphics;
 mod layout;
 mod window;
+mod window_proc;
 pub mod world;
 
 pub use app::*;
 pub use graphics::*;
 pub use layout::*;
 pub use window::{
-    Window, WindowHandle, create_windows, ecs_wndproc, get_entity_from_hwnd, set_ecs_world,
+    Window, WindowHandle, create_windows,
 };
+pub use window_proc::{ecs_wndproc, get_entity_from_hwnd, set_ecs_world};
