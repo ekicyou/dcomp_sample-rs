258da93b0a8d29884fe5268cf72e290ceaf0fe31
ekicyou <dot.station@gmail.com>
2025-11-30 20:42:00 +0900

    Remove window position debug log file to clean up repository

--- test_debug.rs
+++
@@ -1,6 +0,0 @@
-use windows::Win32::Graphics::DirectComposition::IDCompositionVisual3;
-fn main() {
-    // Check if Debug is implemented
-    fn assert_debug<T: std::fmt::Debug>() {}
-    assert_debug::<IDCompositionVisual3>();
-}
--- test_opacity.rs
+++
@@ -1,4 +0,0 @@
-use windows::Win32::Graphics::DirectComposition::IDCompositionVisual;
-fn test(v: &IDCompositionVisual) {
-    unsafe { v.SetOpacity(0.5); }
-}
